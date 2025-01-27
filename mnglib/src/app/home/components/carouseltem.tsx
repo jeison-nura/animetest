b775f73926395ec0923516c3fafb212ae00a5fc0
unknown <jeisonnoc@gmail.com>
2025-01-27 14:13:34 +1100

    Initial commit

---
+++ mnglib/src/app/home/components/carouseltem.tsx
@@ -0,0 +1,25 @@
+"use client";
+import Image from "next/image";
+import React from "react";
+
+export const CarouselItem = ({ item, isActive }) => {
+  console.log(item, "hi");
+  console.log("llegue a este componente");
+  return (
+    <div
+      className={`relative flex-shrink-0 w-full h-64 md:h-96 transition-opacity duration-300 ${
+        isActive ? "opacity-100" : "opacity-0 absolute top-0 left-0"
+      }`}
+    >
+      <Image
+        src={item.url}
+        className="w-full h-full object-cover rounded-lg"
+        alt={item.name}
+        priority
+      />
+      <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black to-transparent p-4">
+        <h3 className="text-black text-xl font-bold">{item.name}</h3>
+      </div>
+    </div>
+  );
+};
