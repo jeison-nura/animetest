fac6c9707efc57a70dc324d0162b1aaf50b80ba6
jeison <jeisonnoc@gmail.com>
2025-10-14 10:29:13 -0500

    Implement GetMangaByID use case and associated routes; add MangaController for handling manga-related requests

--- backend/upload-service/wire.go
+++ backend/upload-service/wire.go
@@ -4,6 +4,8 @@
 package main
 
 import (
+	"github.com/animetest/backend/upload-service/internal/application/routes"
+	"github.com/animetest/backend/upload-service/internal/application/useCases"
 	"github.com/animetest/backend/upload-service/internal/infrastructure/config"
 	"github.com/animetest/backend/upload-service/internal/infrastructure/mockData"
 	"github.com/gin-gonic/gin"
@@ -12,9 +14,16 @@ import (
 
 func InitializeApp(cfg config.Config, router *gin.Engine) (*gin.Engine, error) {
 	wire.Build(
+		// Repositories
 		mockData.NewMockMangaRepository,
 		mockData.NewMockChapterRepository,
 		mockData.NewMockVolumeRepository,
+
+		// Use Cases
+		useCases.NewGetMangaByIDUseCase,
+
+		// Routes
+		routes.SetupRoutes,
 	)
 	return router, nil
 }
